db9aa99671c6b2ea812fb50ca520bc59ee920139
fathil11 <fathil.arham@gmail.com>
2020-06-30 13:08:46 +0700

    Update admin seeder

--- database/seeds/AdminSeeder.php
+++ database/seeds/AdminSeeder.php
@@ -39,8 +39,8 @@ class AdminSeeder extends Seeder
         ]);
 
         User::create([
-            'nip' => '007007007',
-            'name' => 'Om Bambang',
+            'nip' => '11111',
+            'name' => 'Admin',
             'user_position_id' => 1,
             'email' => 'admin@dinkesmelawi.com',
             'phone_number' => '082225210125',
@@ -49,12 +49,32 @@ class AdminSeeder extends Seeder
         ]);
 
         User::create([
-            'nip' => '007007007',
-            'name' => 'Sri Wahyuni',
-            'user_position_id' => 4,
+            'nip' => '22222',
+            'name' => 'Kepala Dinas',
+            'user_position_id' => 2,
+            'email' => 'kepaladinas@dinkesmelawi.com',
+            'phone_number' => '082225210125',
+            'username' => 'kepala_dinas',
+            'password' => Hash::make('123123'),
+        ]);
+
+        User::create([
+            'nip' => '33333',
+            'name' => 'Sekretaris',
+            'user_position_id' => 3,
             'email' => 'sekretaris@dinkesmelawi.com',
             'phone_number' => '082225210125',
-            'username' => 'admin',
+            'username' => 'sekretaris',
+            'password' => Hash::make('123123'),
+        ]);
+
+        User::create([
+            'nip' => '44444',
+            'name' => 'Kepala TU',
+            'user_position_id' => 4,
+            'email' => 'kepalatu@dinkesmelawi.com',
+            'phone_number' => '082225210125',
+            'username' => 'kepala_tu',
             'password' => Hash::make('123123'),
         ]);
     }
