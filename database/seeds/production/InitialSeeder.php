ccf0ee3a43fcd2f54dc7a50f1b55bfa6e71b42ba
fathil11 <fathil.arham@gmail.com>
2020-07-09 06:47:37 +0700

    Add more mail type, mail correction type

--- database/seeds/production/InitialSeeder.php
+++ database/seeds/production/InitialSeeder.php
@@ -20,20 +20,146 @@ class InitialSeeder extends Seeder
     {
         // Mail Type
         MailType::create([
-            "type" => 'Undangan',
-            "code" => 'UDG',
-            "color" => '#ffd500',
-        ]);
-
-        MailType::create([
             "type" => 'Permohonan',
             "code" => 'PRMHN',
             "color" => '#1bcfb4',
         ]);
 
         MailType::create([
-            "type" => 'Peminjaman',
-            "code" => 'PMNJMN',
+            "type" => 'Pemberitahuan',
+            "code" => 'PBRTHN',
+            "color" => '#ffd500',
+        ]);
+
+        MailType::create([
+            "type" => 'Keterangan',
+            "code" => 'KTRGN',
+            "color" => '#1bcfb4',
+        ]);
+
+        MailType::create([
+            "type" => 'Undangan',
+            "code" => 'UDG',
+            "color" => '#c3bdbd',
+        ]);
+
+        MailType::create([
+            "type" => 'Janji Temu',
+            "code" => 'JNJTM',
+            "color" => '#c3bdbd',
+        ]);
+       
+        MailType::create([
+            "type" => 'Balas Janji Temu',
+            "code" => 'BLSJNJTM',
+            "color" => '#c3bdbd',
+        ]);
+
+        MailType::create([
+            "type" => 'Ucapan Terimakasih',
+            "code" => 'TRMKSH',
+            "color" => '#c3bdbd',
+        ]);
+
+        MailType::create([
+            "type" => 'Ucapan Duka Cita',
+            "code" => 'DKCT',
+            "color" => '#c3bdbd',
+        ]);
+
+        MailType::create([
+            "type" => 'Permohonan Bantuan',
+            "code" => 'PMHBNT',
+            "color" => '#c3bdbd',
+        ]);
+        
+        MailType::create([
+            "type" => 'Pemberian Bantuan',
+            "code" => 'PMBBNT',
+            "color" => '#c3bdbd',
+        ]);
+
+        MailType::create([
+            "type" => 'Permohonan Izin',
+            "code" => 'PMHIZN',
+            "color" => '#c3bdbd',
+        ]);
+
+        MailType::create([
+            "type" => 'Pemberian Izin',
+            "code" => 'PMBIZN',
+            "color" => '#c3bdbd',
+        ]);
+
+        MailType::create([
+            "type" => 'Tugas',
+            "code" => 'TGS',
+            "color" => '#c3bdbd',
+        ]);
+
+        MailType::create([
+            "type" => 'Perintah Kerja',
+            "code" => 'PRTKRJ',
+            "color" => '#c3bdbd',
+        ]);
+
+        MailType::create([
+            "type" => 'Perjalanan Dinas',
+            "code" => 'PRJDNS',
+            "color" => '#c3bdbd',
+        ]);
+
+        MailType::create([
+            "type" => 'Berita Acara',
+            "code" => 'BA',
+            "color" => '#c3bdbd',
+        ]);
+
+        MailType::create([
+            "type" => 'Edaran',
+            "code" => 'EDRN',
+            "color" => '#c3bdbd',
+        ]);
+
+        MailType::create([
+            "type" => 'Laporan',
+            "code" => 'LPRN',
+            "color" => '#c3bdbd',
+        ]);
+
+        MailType::create([
+            "type" => 'Pengantar',
+            "code" => 'PNGTR',
+            "color" => '#c3bdbd',
+        ]);
+
+        MailType::create([
+            "type" => 'Referensi',
+            "code" => 'RFRNS',
+            "color" => '#c3bdbd',
+        ]);
+
+        MailType::create([
+            "type" => 'Rekomendasi',
+            "code" => 'RKMNDS',
+            "color" => '#c3bdbd',
+        ]);
+
+        MailType::create([
+            "type" => 'Peringatan',
+            "code" => 'PRNGTN',
+            "color" => '#c3bdbd',
+        ]);
+
+        MailType::create([
+            "type" => 'Panggilan',
+            "code" => 'PGLN',
+            "color" => '#c3bdbd',
+        ]);
+
+        MailType::create([
+            "type" => 'Kuasa',
+            "code" => 'KSA',
             "color" => '#c3bdbd',
         ]);
 
@@ -102,15 +228,23 @@ class InitialSeeder extends Seeder
 
         // Mail Correction Type
         MailCorrectionType::create([
-            'type' => 'Kesalahan KOP'
+            'type' => 'Kesalahan Kepala Surat'
         ]);
 
         MailCorrectionType::create([
-            'type' => 'Kesalahan Perihal'
+            'type' => 'Kesalahan Penulisan Lampiran'
         ]);
 
         MailCorrectionType::create([
-            'type' => 'Kesalahan Nama'
+            'type' => 'Kesalahan Tanggal'
+        ]);
+
+        MailCorrectionType::create([
+            'type' => 'Kesalahan ALamat'
+        ]);
+
+        MailCorrectionType::create([
+            'type' => 'Kesalahan Pembuka / Penutup'
         ]);
     }
 }
